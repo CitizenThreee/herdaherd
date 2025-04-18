fe33194fc74bb3cf616740baf0e99ee9ba8ea34d
pkhokgawe <pkhokgawe@atlassian.com>
2025-04-18 13:28:23 +1200

    sheep

--- Assets/Scripts/SheepManager.cs
+++ Assets/Scripts/SheepManager.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class SheepManager : MonoBehaviour
 {
+    
+    [SerializeField] GameObject sheepRef;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < 100; i++)
+        {
+            Vector3 pos = new Vector3(i, transform.position.y, i);
+            Instantiate(sheepRef, pos, transform.rotation);
+        }
     }
 
     // Update is called once per frame
